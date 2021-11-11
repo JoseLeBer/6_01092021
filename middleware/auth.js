1203c8625912cdb71ea5441ef81ce7980bf9c9fd
José Le Ber <jose.ruiz.lb@gmail.com>
2021-11-11 15:24:15 +0100

    Correction dotenv

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,15 +1,12 @@
-// Création du fichier "auth.js" où on vérifie le token envoyé dans la requête de l'application front-end
-/// On vérifie que c'est un token valable et aussi que, si il y a un userId qui est envoyé avec la requête il corresponde à celui encodé dans le token
-
-// On importe le package "jsonwebtoken" qui permet de créer et vérifier les tokens
 const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 module.exports = (req, res, next) => {
   try {
     // On récupère le token dans le header "authorization"
     const token = req.headers.authorization.split(" ")[1];
     // Puis on décrypte le token avec la fonction "verify" de jwt
-    const decodedToken = jwt.verify(token, "RANDOM_TOKEN_SECRET");
+    const decodedToken = jwt.verify(token, process.env.SECRET_TOKEN);
     // On récupère le userId contenu dans le token
     const userId = decodedToken.userId;
     // Si il y a un userId dans le corps de la requête && qu'il est différent de celui du token
